ab65d78649165322cd2cc38755a6ecebd3142725
shavaun1 <jshavaun@yahoo.com>
2022-04-22 17:24:13 -0400

    new commit

--- Exercise12.cpp
+++ Exercise12.cpp
@@ -23,4 +23,33 @@ int main() {
 	//double = dval 3.14;
 	//int &refval5 = davl;	//error: initilizer must be an int object 
 
+	const int bufSize = 512;				//input buffer size
+	//bufSize = 512;				//error: attempt to write to const object
+
+	//const int i = get_size();		//ok: initialize at run time
+	const int j = 42;				//ok: initialize at compile time
+	//const int k;					//error: k is unitialize const
+
+	int i = 42;
+	const int ci = i;				//ok: the value in i is copied into ci
+	int j = ci;						//ok: the value in ci is copied into j
+
+	const int ci = 1024;
+	const int& r1 = ci;				//ok: both references and underlying object
+	//r1 = 42;						//error: r1 is a reference to const
+	//int &r2 = ci;					//error: non const reference to a const object
+
+
+	int i = 42;
+	const int& r1 = i;				//we can bind a const int& to a plain int object
+	const int& r2 = 42;				//ok: r1 is a reference to const
+	const int& r3 = r1 * 2;			//ok: r3 is a reference to const
+	//int& r4 = r * 2;				//error: r4 is a plain non const reference
+
+	int i = 42;
+	//int& r1 = i;					//r1 bound to i
+	const int& r2 = i;				//r2 also bound to i; but cannot be used to changed i.
+	//r1 = 0;							//r1 is not const; i is now 0
+	//r2 = 0;						//error: r2 is a reference to const	
+
 }
